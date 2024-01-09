d861788ac2c0f7c2ba0631c3f2b1c4ce862795bc
Amith-Jagannath <amithjagannath02@gmail.com>
2024-01-09 21:46:38 +0530

    vercel

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -5,7 +5,8 @@ const User = g
     name: g.string().length({ min: 2, max: 20 }),
     email: g.string().unique(),
     avatarUrl: g.url(),
-    performs: g.relation(() => Perform).optional(),
+    performs: g.relation((): any => Perform).optional(),
+    posts: g.relation((): any => Post).optional(),
   })
   .auth((rules) => {
     rules.public().read();
@@ -23,11 +24,24 @@ const Perform = g
     rules.private().create().delete().update();
   });
 
-
-  const jwt = auth.JWT({
-    issuer: "grafbase",
-    secret: process.env.NEXTAUTH_SECRET!,
+const Post = g
+  .model("Post", {
+    title: g.string(),
+    description: g.string(),
+    image: g.string(),
+    date: g.string(),
+    reference: g.string(),
+    created: g.relation(() => User),
+  })
+  .auth((rules) => {
+    rules.public().read();
+    rules.private().create().delete().update();
   });
+
+const jwt = auth.JWT({
+  issuer: "grafbase",
+  secret: process.env.NEXTAUTH_SECRET!,
+});
 
 export default config({
   schema: g,
